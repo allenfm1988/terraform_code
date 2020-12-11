fc5062c08e3831d1ca8dc01d73e633172461c3ff
Allen <elbarto0488@gmail.com>
2020-12-11 06:49:31 -0600

    RDS oracle code v7

--- complete-oracle/main.tf
+++ complete-oracle/main.tf
@@ -22,7 +22,7 @@ data "aws_security_group" "default" {
 # DB
 #####
 module "db" {
-  source = "../modules/db_instance"
+  source = "./modules/db_instance"
 
   identifier = "demodb-oracle-ulacit"
 
@@ -59,7 +59,7 @@ module "db" {
   family = "default.oracle-se1-11.2"
 
   # DB option group
-  #major_engine_version = "11.2"
+  major_engine_version = "11.2"
 
   # Snapshot name upon DB deletion
   final_snapshot_identifier = "demodbula"
